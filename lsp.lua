d7257e3aa4519cd6f756a1d54b3479db82b9808b
cohama <cohama@live.jp>
2022-08-17 01:59:11 +0900

    Update lsp.buf.format

--- lsp.lua
+++ lsp.lua
@@ -35,7 +35,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '\\nq', '<cmd>lua vim.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap('n', '\\nf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_set_keymap('n', '\\nf', '<cmd>lua vim.lsp.buf.format { async = true }<CR>', opts)
 
 end
 
