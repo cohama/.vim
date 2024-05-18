a89c22a7e79c95261a14299237fa4e97f630676b
cohama <cohama@live.jp>
2024-05-18 13:23:29 +0900

    Update run_pylsp

--- run_pylsp.sh
+++ run_pylsp.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -eu
-python_paths=$(python -c "import sys; print(sys.path[-1])")
-python_version=$(python -c "import sys; print(*sys.version_info[:2], sep='.')")
+if [[ -x $(command -v pipx) && -x $(command -v pylsp) ]] ; then
+  python_cmd=$(pipx environment -v PIPX_LOCAL_VENVS)/python-lsp-server/bin/python
+else
+  python_cmd=python
+fi
+python_paths=$("$python_cmd" -c "import sys; print(':'.join(sys.path[-2:]))")
+python_version=$("$python_cmd" -c "import sys; print(*sys.version_info[:2], sep='.')")
 cache_dir=${XDG_CACHE_HOME:-"$HOME/.cache"}/nvim/pylsp
 mkdir -p "$cache_dir"
+echo python_paths="$python_paths" >&2
 exec env "PYTHONPATH=${python_paths}:.venv/lib/python${python_version}/site-packages" pylsp -vv --log-file "$cache_dir/lsp.log"
